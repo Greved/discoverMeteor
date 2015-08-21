425ab13a77ba480d0b13fcc1b4798f4e715a5d66
Andrey <jokeposter@yandex.ru>
2015-08-21 23:41:33 +0500

    server postInsert

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -1,3 +1,8 @@
 Template.layout.helpers({
 	pageTitle: function() { return Session.get('pageTitle'); }
 });
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    console.log('There are ' + Posts.find().count() + ' posts');
+  });
+});
